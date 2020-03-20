63131bee65fd035bb47740f60b258be053bf118c
Siradji Awoual <surajauwal89@gmail.com>
2020-03-20 13:55:26 +0100

    FIrst commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+  <div className="app">
+    <div className="section">
+      <h1 className="heading">React App Skelaton</h1>
+      {/* <div className="inner">
+        <p className="small-text">A very powerful alternative to create react app</p>
+      </div> */}
+    </div>
+  </div>
+)
+
+export default App
